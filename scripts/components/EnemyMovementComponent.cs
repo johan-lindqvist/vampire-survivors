316e4ef4a308c5a5775a1018339207a5d66132e0
Johan Lindqvist <jlt.johanlindqvist@gmail.com>
2025-02-04 21:57:29 +0100

    fix: components not working

--- scripts/components/EnemyMovementComponent.cs
+++ scripts/components/EnemyMovementComponent.cs
@@ -4,10 +4,13 @@ namespace VampireSurvivors.scripts.components;
 
 public partial class EnemyMovementComponent : Node2D
 {
-    [Export] public float Speed = 100f;
-    
-    public override void _PhysicsProcess(double delta)
-    {
-        Position = Position.MoveToward(Player.Instance.Position, (float)delta * Speed);
-    }
+	[Export] public float Speed = 100f;
+
+	[Export]
+	private Node2D enemy;
+
+	public override void _PhysicsProcess(double delta)
+	{
+		enemy.Position = enemy.Position.MoveToward(Player.Instance.Position, (float)delta * Speed);
+	}
 }
--- scripts/components/HitboxComponent.cs
+++ scripts/components/HitboxComponent.cs
@@ -4,10 +4,10 @@ namespace VampireSurvivors.scripts.components;
 
 public partial class HitboxComponent : Area2D
 {
-    [Export] private HealthComponent healthComponent;
+	[Export] private HealthComponent healthComponent;
 
-    public float Damage(float damage)
-    {
-        return healthComponent.Damage(damage);
-    }
+	public float Damage(float damage)
+	{
+		return healthComponent.Damage(damage);
+	}
 }
